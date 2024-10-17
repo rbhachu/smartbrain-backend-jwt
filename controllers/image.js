4c0ebf09eb3eb855d14a0ee6bce3fec98c2ce921
Rishi Bhachu <mr_bhachu@hotmail.com>
2024-10-17 17:12:57 +0100

    revert import approach

--- controllers/image.js
+++ controllers/image.js
@@ -1,11 +1,23 @@
-import { App as ClarifaiApp } from 'clarifai';
+const Clarifai = require('clarifai');
 
 // Initialize with the API key directly
-const app = new ClarifaiApp({
+const app = new Clarifai.App({
   apiKey: process.env.API_CLARIFAI
 });
 
-export const handleImage = (req, res, db) => {
+const handleApiCall = (req, res) => {
+  app.models
+    .predict({id: Clarifai.FACE_DETECT_MODEL}, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      console.log('Handle API Error: ', err);
+      res.status(400).json('Handle API Error');
+    });
+};
+
+const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users')
     .where('id', '=', id)
@@ -20,14 +32,7 @@ export const handleImage = (req, res, db) => {
     });
 };
 
-export const handleApiCall = (req, res) => {
-  app.models
-    .predict({ id: Clarifai.FACE_DETECT_MODEL }, req.body.input)
-    .then(data => {
-      res.json(data);
-    })
-    .catch(err => {
-      console.log('Handle API Error: ', err);
-      res.status(400).json('Handle API Error');
-    });
+module.exports = {
+  handleImage,
+  handleApiCall
 };
