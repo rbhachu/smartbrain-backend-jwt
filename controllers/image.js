11b40e49c6c69e7a4e0db631d365cd970121d55e
Rishi Bhachu <mr_bhachu@hotmail.com>
2024-10-17 16:46:20 +0100

    update call to api

--- controllers/image.js
+++ controllers/image.js
@@ -1,42 +1,32 @@
 const Clarifai = require('clarifai');
 
-//You must add your own API key here from Clarifai.
-const app = new Clarifai.App({ 
-  apiKey: process.env.API_CLARIFAI // dynamic key value stored on Heroku servers for security. You can also replace this value with your own key from Clarifai too.
+// Initialize with the API key directly
+const app = new Clarifai.App({
+  apiKey: process.env.API_CLARIFAI
 });
 
 const handleApiCall = (req, res) => {
   app.models
-    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input) // get body input
-    .then(data => { 
+    .predict({id: Clarifai.FACE_DETECT_MODEL}, req.body.input)
+    .then(data => {
       res.json(data);
     })
-    .catch(err => res.status(400).json('unable to work with API')) // catch error
-  }
-  
-  // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
-  // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
-  // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
-  // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
-  // is to use a different version of their model that works like: `c0c0ac362b03416da06ab3fa36fb58e3`
-  // so you would change from:
-  // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-  // to:
-  // .predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input)
-  
+    .catch(err => res.status(400).json('unable to work with API'));
+};
 
 const handleImage = (req, res, db) => {
   const { id } = req.body;
-  db('users').where('id', '=', id)
-  .increment('entries', 1)
-  .returning('entries')
-  .then(entries => {
-    res.json(entries[0]);
-  })
-  .catch(err => res.status(400).json('unable to get entries'))
-}
+  db('users')
+    .where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('unable to get entries'));
+};
 
 module.exports = {
   handleImage,
   handleApiCall
-}
+};
